7a842e0bd5150ddb813522a852265c9aa164ac85
Alexander Volkov <agvolkov5@gmail.com>
2024-10-01 03:44:20 +0300

    refactor: building process

--- vite.config.js
+++ vite.config.js
@@ -5,82 +5,47 @@ import legacy from "@vitejs/plugin-legacy"
 import viteCompression from "vite-plugin-compression"
 import { visualizer } from "rollup-plugin-visualizer"
 
-// const outputPath = path.resolve("./public/")
-const PRODUCTION = process.env.NODE_ENV === "production"
-const indexHtmlPath = PRODUCTION ? "src/index-prod.html" : "src/index-dev.html"
-console.log("MOD PRODUCTION=", PRODUCTION, "indexHtmlPath=", indexHtmlPath)
+export default defineConfig(({ mode }) => {
+    const __production__ = mode === "production"
+    const indexHtmlPath = __production__ ? "src/index-prod.html" : "src/index-dev.html"
+    console.log("MOD PRODUCTION=", __production__, "indexHtmlPath=", indexHtmlPath)
 
-export default defineConfig({
-    root: "src",
-    base: PRODUCTION ? "/geodarts/" : "/",
-    build: {
-        outDir: "../dist",
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes("node_modules")) {
-                        return id.toString().split("node_modules/")[1].split("/")[0]
-                    }
-                },
+    return {
+        root: "src",
+        base: __production__ ? "/test/geodarts/" : "/",
+        build: {
+            outDir: "../dist",
+        },
+        plugins: [
+            react(), // Handles React fast refresh
+            __production__ && legacy({
+                targets: ["defaults", "not IE 11"],
+            }),
+            // viteCompression({
+            //     algorithm: "gzip",
+            //     threshold: 10240,
+            //     minRatio: 0.8,
+            // }),
+            // visualizer({ open: true }) // Equivalent to webpack-bundle-analyzer
+        ].filter(Boolean),
+
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"), // Alias for src
             },
         },
-    },
-    plugins: [
-        react(), // Handles React fast refresh
-        PRODUCTION && legacy({
-            targets: ["defaults", "not IE 11"],
-        }),
-        viteCompression({
-            algorithm: "gzip",
-            threshold: 10240,
-            minRatio: 0.8,
-        }),
-        visualizer({ open: true }) // Equivalent to webpack-bundle-analyzer
-    ].filter(Boolean),
 
-    // build: {
-    //     rollupOptions: {
-    //         input: "src/index.tsx",
-    //         output: {
-    //             entryFileNames: PRODUCTION ? "[name]-[hash].js" : "main.js",
-    //             assetFileNames: "assets/[name]-[hash].[ext]",
-    //             manualChunks(id) {
-    //                 if (id.includes("node_modules")) {
-    //                     return "vendor"
-    //                 }
-    //             }
-    //         }
-    //     },
-    //     sourcemap: PRODUCTION ? "source-map" : "inline",
-    //     minify: PRODUCTION ? "terser" : false,
-    // },
-
-    // css: {
-    //     preprocessorOptions: {
-    //         scss: {
-    //             // If you need global variables or mixins in SCSS
-    //             additionalData: "@import \"./src/styles/variables.scss\";"
-    //         }
-    //     }
-    // },
-
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"), // Alias for src
+        server: {
+            host: "0.0.0.0",
+            port: 3000,
+            open: true,
+            hmr: true,
         },
-        // extensions: [".js", ".ts", ".jsx", ".tsx"]
-    },
 
-    server: {
-        host: "0.0.0.0",
-        port: 3000,
-        open: true,
-        hmr: true,
-    },
-
-    preview: {
-        host: "0.0.0.0",
-        port: 3001,
-        strictPort: true
+        preview: {
+            host: "0.0.0.0",
+            port: 3001,
+            strictPort: true
+        }
     }
 })
