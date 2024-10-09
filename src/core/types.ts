e7c39934aa989a64afaaa65d00738849cc9aac0a
Alexander Volkov <agvolkov5@gmail.com>
2024-10-09 23:52:00 +0300

    style: remove semicolons

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,13 +1,13 @@
 import { LngLat } from "mapbox-gl"
 
 export interface City {
-    name: string;
-    id: number;
-    ll: LngLat;
-    wikilink?: string;
-    radius?: number;
+    name: string,
+    id: number,
+    ll: LngLat,
+    wikilink?: string,
+    radius?: number,
     attempted?: {
-        direction: number;
-        distanceKm: number;
-    };
+        direction: number,
+        distanceKm: number
+    }
 }
